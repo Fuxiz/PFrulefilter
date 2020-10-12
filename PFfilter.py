4cac8249ac2e6bb344fc8e9689ff43a951e56cdd
Fuxiz <file-stef@hotmail.com>
2020-10-12 22:48:29 +0200

    Refining the output

--- PFfilter.py
+++ PFfilter.py
@@ -1,32 +1,52 @@
 import re 
 
-syntaxlist = [['pass','block'],['in','out'],['log'],['quick'],['egress','vmx0'],['inet','inet6'],['tcp','udp','icmp','icmp6','http','https'],['from'],['to'],['port'],['flags S/SA'],['no state','keep state','modulate state','synproxy state']]
-macrohost = ""
-islist = False
-filteredRules = []
-with open('pfrule.txt','r') as f:
-    for line in f:
-        # result = re.search(r"(?<=$)\w+",line)
-        # print('result it',result.group(0))
-        rules = line.split()
-        rulelen = len(rules)
-        for s in range(rulelen):
-            if rules[s] == '{':
-                islist = True
-            elif rules[s] == '}':
-                islist = False
-                print(macrohost.strip('{'))
-                s += 1
-                macrohost = ''
-            if islist == True:
-                macrohost += rules[s]
-                macrohost += ' '
-            else:
-                for rulecount in range(len(syntaxlist)):   
+def main():
+    syntaxlist = [['pass','block'],['in','out'],['log'],['quick'],['egress','vmx0'],['inet','inet6'],['tcp','udp','icmp','icmp6','http','https'],['from'],['to'],['port'],['flags S/SA'],['no state','keep state','modulate state','synproxy state']]
+    macrohost = ""
+    islist = False
+    filteredRules = []
+    with open('pfrule.txt','r') as f:
+        for line in f:
+            # result = re.search(r"(?<=$)\w+",line)
+            # print('result it',result.group(0))
+            rules = line.split()
+            rulelen = len(rules)
+            for s in range(rulelen):
+                if rules[s] == '{':
+                    islist = True
+                    continue
+                elif rules[s] == '}':
+                    islist = False
+                    s += 1
+                    macrohost = ''
+                elif re.search('\$.+', rules[s]):
+                    print(findhost(rules[s].strip('$')))
+                    continue 
+                if islist == True:
+                    if re.search('\$.+', rules[s]) is None:
                         print(rules[s])
-                        rulecount += 1
-                        break
-                        for listitem in range(len(syntaxlist[rulecount])):
-                                if rules[s] == syntaxlist[rulecount][listitem]:
-                                   print(rules[s])
-        print("\n")
+                        continue 
+                    onehost = findhost(rules[s])
+                    macrohost += onehost
+                    macrohost += ' '
+                else:
+                    for rulecount in range(len(syntaxlist)):   
+                            print(rules[s])
+                            rulecount += 1
+                            break
+                            for listitem in range(len(syntaxlist[rulecount])):
+                                    if rules[s] == syntaxlist[rulecount][listitem]:
+                                       print(rules[s])
+            print(",")
+
+def findhost(hostname):
+    with open('hosts.txt','r') as hostline:
+        for line in hostline:
+            # hostname = 'test2'
+            result = 'none'
+            regex = rf'(?<={hostname}\=\")\d.{{7}}'
+            for match in re.finditer(regex,line):
+                result = match.group(0)
+                return(result)            
+    return(result)
+main()
--- findhost..py
+++ findhost..py
@@ -1,12 +1,23 @@
 import re
 
-test = 'test2="10.2.0.1"'
-hostname = 'test2'
-regex = rf'(?<={hostname}\=\")\d.{{7}}'
-for match in re.finditer(regex,test):
-    s = match.start()
-    e = match.end()
-# result = re.finditer(regex,test,re.M|re.I )
-    print('string is',test,'start is',s,'end is',e,'and result is',match.group(0))
+# test = 'test2="10.2.0.1"'
+# hostname = 'test2'
+# regex = rf'(?<={hostname}\=\")\d.{{7}}'
+# for match in re.finditer(regex,test):
+    # s = match.start()
+    # e = match.end()
+# # result = re.finditer(regex,test,re.M|re.I )
+    # print('string is',test,'start is',s,'end is',e,'and result is',match.group(0))
 
-print(test)
+# print(test)
+with open('hosts.txt','r') as hostline:
+    for line in hostline:
+        hostname = 'test2'
+        regex = rf'(?<={hostname}\=\")\d.{{7}}'
+        for match in re.finditer(regex,line):
+            s = match.start()
+            e = match.end()
+        # result = re.finditer(regex,test,re.M|re.I )
+            print(match.group(0))
+
+print(line)
